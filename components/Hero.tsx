7e600d7d83c96e54c7869809fe59e58083ea0fb8
Erik Vroon <erik.vroon22@gmail.com>
2025-03-04 10:17:36 +0100

    Create dependabot.yml

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,5 @@
-import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
 import { HeroCards } from "./HeroCards";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { IconLibrary, IconRocket } from "@tabler/icons-react";
 
